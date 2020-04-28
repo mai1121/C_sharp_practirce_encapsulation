1270c6b842551b11b72721d17affadc21139209a
mai1121 <maimaiukawa@gmail.com>
2020-04-28 19:50:46 +0900

    indexer

--- practice_encapsulation/Program.cs
+++ practice_encapsulation/Program.cs
@@ -15,9 +15,27 @@ namespace practice_encapsulation
             b1.FirstName = "村上";
             b1.LastName = "春樹";
             Console.WriteLine(b1.Name());
+
+            var m = new Member();
+            //インデクサーにアクセス
+            var memberList = new string[] {"ゆきこ","愛子","皐月","春子","昌美" };
+            for(var i = 0; i < 5; i++)
+            {
+                m[i] = memberList[i];
+                Console.WriteLine(m[i]);
+            }
+
+            var p = new Price(4);
+            p[0] = 390;
+            p[1] = 230;
+            p[2] = 500;
+            p[3] = 450;
+
+            Console.WriteLine($"合計で{p.Total()}円");
         }
     }
 
+    #region BMIクラス定義（プロパティ練習用）
     class BMI
     {
         //フィールドはprivate権限で定義
@@ -68,4 +86,49 @@ namespace practice_encapsulation
 
 
     }
+    #endregion
+
+    class Member
+    {
+        //privateなフィールド定義、初期化する
+        private string[] member=new string[5];
+
+        //インデクサー
+        public string this[int i]
+        {
+            set { this.member[i] = value; }
+            get { return this.member[i]; }
+
+        }
+    }
+
+    class Price
+    {
+        private int size; //配列サイズをフィールド定義
+        private int[] list; //空の配列をフィールド定義
+
+        //コンストラクター（フィールド初期化）
+        public Price(int size)
+        {
+            this.size = size;
+            this.list = new int[size];
+        }
+        //インデクサー
+        public int this[int index]
+        {
+            set { this.list[index] = value; }
+            get { return this.list[index]; }
+        }
+        //インデクサーでセットしたフィールド値使って合計金額計算するメソッド
+        public int Total()
+        {
+            int total = 0;
+            foreach(var i in list)
+            {
+                total += i;
+            }
+            return total;
+        }
+    }
+
 }
